85847a91202330c46d66e11a0a73750b148459ab
Maudite Machine <mauditemachine@mac.home>
2025-08-21 02:39:29 -0400

    fix(prod): switch to relative base './', update docs and add .nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/MauditeMachine/' : '/',
+  base: command === 'build' ? './' : '/',
   server: { port: 5173, open: true },
   publicDir: 'medias'
 }))
