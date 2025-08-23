d51210d16e71a7da292861c701abfdeaf78d9225
Maudite Machine <mauditemachine@mac.home>
2025-08-23 01:43:28 -0400

    fix: Restore Instagram feed in Medias section

--- src/components/Medias.tsx
+++ src/components/Medias.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import InstagramFeed from './InstagramFeed';
 
 interface MediasProps {
   onBackgroundChange?: (url: string) => void;
@@ -7,18 +8,8 @@ interface MediasProps {
 const Medias: React.FC<MediasProps> = ({ onBackgroundChange }) => {
   return (
     <div className="medias-container">
-      <h2 style={{ color: 'white', marginBottom: '20px' }}>MEDIAS - PLAYLIST</h2>
       <div className="section-scroll">
-        <div style={{ width: '100%', height: '600px' }}>
-          <iframe 
-            width="100%" 
-            height="600" 
-            scrolling="no" 
-            frameBorder="no" 
-            allow="autoplay" 
-            src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/mauditemachine/sets/tracks-1&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-          />
-        </div>
+        <InstagramFeed />
       </div>
     </div>
   );
